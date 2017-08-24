8cc7df46f036b68148914a29ebc41bbc6962f425
Wenda Ni, Ph.D <wonda.ni@gmail.com>
2017-08-23 23:56:02 -0400

    Corrections: ';' not ',' in struct definition
    ^2, no such operator in C
    
    Corner case that all four points overlap
    
    	modified:   square.c

--- square.c
+++ square.c
@@ -1,27 +1,76 @@
-struct cord{
-    int x,
-    int y,
+#include <stdio.h>
+
+enum {
+	false = 0,
+	true  = 1,
 };
 
-int is_square(struct cord[])
+struct coord{
+    int x;
+    int y;
+};
+
+void inser_sort(int a[], int size)
 {
-    int dist[6] = {0};
+	int i, j, temp;
+
+	if ((!a) || (size < 2))
+		return;
+
+	for (i = 1; i < size; i++) {
+		temp = a[i];
+		for (j = i - 1; j >= 0 && temp < a[j]; j--)
+			a[j + 1] = a[j];
+		a[j + 1] = temp;
+	}
+}
+
+int is_square(struct coord point[])
+{
+    int dist_sq[6] = {0};
+    int i, j;
     int idx = 0;
 
-    for (i = 0; i < 4, i++)
+    for (i = 0; i < 4; i++)
         for (j = i + 1; j < 4; j++) {
-            dist[idx] = (cord[i].x - cord[j].x)^2 + (cord[i].y - cord[j].y)^2;
+            dist_sq[idx] = (point[i].x - point[j].x) * (point[i].x - point[j].x)
+				+ (point[i].y - point[j].y) * (point[i].y - point[j].y);
+	    if (!dist_sq[idx])
+		return false;
             idx++;
         }
    
-    sort(dist);
+    inser_sort(dist_sq, 6);
    
     for(i = 1; i < 4; i++)
-        if (dist[i] != dist[0])
+        if (dist_sq[i] != dist_sq[0])
             return false;
-    for(i = 4; i< 6; i++)
-        if (dist[i] != 2 * dist[0])
+
+    for(i = 4; i < 6; i++)
+        if (dist_sq[i] != 2 * dist_sq[0])
             return false;
            
     return true;
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	struct coord pnt[] = {
+		{0, 0},
+		{0, 1},
+		{1, 0},
+		{1, 1},
+	};
+
+	printf("is square? %s\n", is_square(pnt) ? "yes" : "no");
+
+	for (i = 0; i < 4; i++) {
+		pnt[i].x = 0;
+		pnt[i].y = 0;
+	}
+
+	printf("is square? %s\n", is_square(pnt) ? "yes" : "no");
+
+	return 0;
 }
